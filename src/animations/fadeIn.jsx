17d621cdbb2c198113838002ba7e46888bdf19bc
Richard van 't Hof <richardvanthof@pm.me>
2019-06-26 22:28:11 +0200

    Added typography.js to GlobalStyle.js
    Now the typography.js configuration will be loaded via Globalstyle.js.
    This makes sure that other processes like text scaling are executed in
    the right order.
    To I did speed up the transitions a bit.

---
+++ src/animations/fadeIn.jsx
@@ -0,0 +1,14 @@
+import {keyframes} from 'styled-components';
+
+const fadeIn = keyframes`
+    @keyframes fadeIn {
+    0% {
+      opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    }
+  }
+`
+
+export default fadeIn;
