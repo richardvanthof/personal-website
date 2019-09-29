6daa271e81c083aa023cadea29e6f90785afa338
laksjfalksjf <slkadjflkadsj@lksdfjalks.com>
2019-09-29 15:30:29 +0200

    Fixed minor merging glitches
    Readded some deleted files so the website would build again.

--- src/components/noScript.jsx
+++ src/components/noScript.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 
-const NoScript = () => (
-  <noscript>
-    <h3>Javascript disabled</h3>
-    <p>
-        For full functionality of this site it is necessary to enable
-        JavaScript. Here are the
-      {' '}
-      <a href="https://www.enable-javascript.com/">
-          instructions how to enable JavaScript in your web browser
-      </a>
-        .
-    </p>
-  </noscript>
-);
+const NoScript = () => {
+    return (
+        <noscript>
+            <h3>Javascript disabled</h3>
+            <p>For full functionality of this site it is necessary to enable JavaScript.
+            Here are the <a href="https://www.enable-javascript.com/">
+            instructions how to enable JavaScript in your web browser</a>.</p>
+        </noscript>
+    )
+}
 
 export default NoScript;
